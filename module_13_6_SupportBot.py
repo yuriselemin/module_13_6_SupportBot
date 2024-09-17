9503184dd5019501fe15d830587570452f17c736
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-09-17 14:37:31 +0300

    Add files via upload

---
+++ module_13_6_SupportBot.py
@@ -0,0 +1,96 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+api = '7318149436:AAGsSUVCDgZhtmeaAEPHaoSxhZDaYpYoO_U'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1)
+kb.add(button2)
+
+
+inline_kb = InlineKeyboardMarkup(row_width=2)
+calories_button = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+formulas_button = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+inline_kb.add(calories_button, formulas_button)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.reply('Привет! Я бот помогающий твоему здоровью.'
+                        '\nВыберите, что Вы хотите сделать:'
+                        '\n',reply_markup=kb)
+
+
+
+@dp.message_handler(text='Рассчитать', state='*')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=inline_kb)
+
+
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('Формула Миффлина-Сан Жеора для расчёта нормы калорий:\n'
+                               'Калории = (10 * вес) + (6.25 * рост) - (5 * возраст) - 161')
+    await call.answer()
+
+
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+    calories = (10 * weight) + (6.25 * growth) - (5 * age) - 161
+    await message.answer(f'Ваша норма калорий: {calories}')
+    await state.finish()
+
+
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
